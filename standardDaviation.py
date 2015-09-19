0415070aebd21bafe46f99cdbe4cf230b3dc887d
saifullah <saifullah@protonmail.ch>
2015-09-19 13:57:09 +0200

    add standard daviation

--- standardDaviation.py
+++ standardDaviation.py
@@ -1,3 +1,28 @@
+import math
 f = open('input.txt','r')
-for i in f.readline():
-	print i
+data = f.readlines()
+d = []
+for line in data:
+	word = (line.strip('\n'))
+	d.append(float(word))
+
+
+def SD(data):
+	avg = mean(data)
+	s = 0
+	for i in data:
+		d = (i-avg)**2
+		s = s + d
+	sigma = s/len(data)
+	return math.sqrt(sigma)
+
+def mean(data):
+	s= 0
+	for i in data:
+		s = s + i
+	avg = s/len(data)
+	return avg
+
+print SD(d)
+	
+
