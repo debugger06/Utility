04f9385804f65a81172c5d12bfdb556862594264
debugger06 <debugger_06@yahoo.com>
2015-11-22 03:24:49 +0100

    mean

--- mean.py
+++ mean.py
@@ -1,5 +1,11 @@
 import random
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+# use ggplot style sheet
+style.use('ggplot')
+
 
 mn = np.zeros(100)
 for i in range(1,100):
@@ -10,4 +16,7 @@ for i in range(1,100):
 		a = random.randint(1,6)
 		if a = 1:
 			count++
-		
+	average = count/if
+	mn[i]=average
+
+plt.plot(mn)
